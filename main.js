452da50edd3a90a59f1c002c61547729782de506
bxs9775 <bxs9775@rit.edu>
2019-03-06 11:45:26 -0500

    Added span charts * Number of players * Playtime

--- main.js
+++ main.js
@@ -6,23 +6,34 @@ var categoryData = {};
 var mechanicData = {};
 
 // Dimensions used by all charts
-let width = 850;
+let width = 1000;
 let height = 850;
-let xPadding = 150;
+let xPadding = 200;
 let yInnerPadding = 0.1;
 let yBottomPadding = 18;
 let xAxisOff = yBottomPadding;
 let yAxisOff = xPadding;
+let spanXAdjust = xPadding;
 
 let color = 'grey';
 
 //data getting functions
+let getId = (d) => d.id;
+
+let getCount = (d) => d.count;
+let getName = (d) => d.name;
+
 let getCategory = (d) => d.category;
 let getMechanic = (d) => d.mechanic;
-let getCount = (d) => d.count;
+
+let getMinPlayers = (d) => d.players.min;
+let getMaxPlayers = (d) => d.players.max;
+let getMinPlaytime = (d) => d.playtime.min;
+let getMaxPlaytime = (d) => d.playtime.max;
 
 //data sorting functions
 let sortCount = (a,b) => b.count-a.count;
+let sortRank = (a,b) => b.rank-a.rank;
 
 //Charting functions
 function createYScale(data,getY){
@@ -34,13 +45,13 @@ function createYScale(data,getY){
   return yScale;
 }
 
-function createAxes(chart,xScale,yScale){
+function createAxes(chart,xScale,yScale,xAdjust=0){
   var xAxis = d3.axisBottom(xScale);
   var yAxis = d3.axisLeft(yScale);
   
   chart.append('g')
     .attr('class', 'xaxis axis')
-    .attr('transform', `translate(${yAxisOff},${height-xAxisOff})`)
+    .attr('transform', `translate(${yAxisOff-xAdjust},${height-xAxisOff})`)
     .call(xAxis);
   
   chart.append('g')
@@ -80,11 +91,37 @@ function createBarChart(data,key,getX,getY){
 }
 
 function createSpanChart(data,key,getMinX,getMaxX,getY){
+  console.log('Create span chart...');
   
+  //Create scales
+  let xMax = d3.max(data,getMaxX);
+  let xScale = d3.scaleLinear()
+    .domain([0,xMax])
+    .rangeRound([xPadding,width]);
+  
+  let yScale = createYScale(data,getY);
+  
+  //Create svg chart
+  let chart = d3.select('#charts').append('svg')
+    .attr('width',width)
+    .attr('height',height);
+  
+  chart.selectAll('rect')
+    .data(data,key)
+    .enter()
+    .append('rect')
+    .attr('x',(d) => xScale(getMinX(d)))
+    .attr('y',(d) => yScale(getY(d)))
+    .attr('width',(d) => xScale(getMaxX(d))-xScale(getMinX(d)))
+    .attr('height',yScale.bandwidth())
+    .attr('fill',color);
+  
+  //Axis
+  createAxes(chart,xScale,yScale,spanXAdjust);
 }
 
 function createCategoryChart(data=categoryData){
-  //console.log('Create category');
+  //console.log('Create category chart');
   //console.dir(data);
   
   data.sort(sortCount);
@@ -93,7 +130,7 @@ function createCategoryChart(data=categoryData){
 }
 
 function createMechanicsChart(data=mechanicData){
-  //console.log('Create mechanics');
+  //console.log('Create mechanics chart');
   //console.dir(data);
   
   data.sort(sortCount);
@@ -102,11 +139,21 @@ function createMechanicsChart(data=mechanicData){
 }
 
 function createPlayersChart(data=dataset){
+  //console.log('Create players chart');
+  //console.dir(data);
   
+  data.sort(sortRank);
+  
+  createSpanChart(data,getId,getMinPlayers,getMaxPlayers,getName);
 }
 
 function createPlaytimeChart(data=dataset){
+  //console.log('Create playtime chart');
+  //console.dir(data);
   
+  data.sort(sortRank);
+  
+  createSpanChart(data,getId,getMinPlaytime,getMaxPlaytime,getName);
 }
 
 function changeChart(){
